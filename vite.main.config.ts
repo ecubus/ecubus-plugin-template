e4a08902e986f6f11dd23a4b2fb7bc709265fe1d
Frankie Zeng <frankie.zengfu@gmail.com>
2025-10-26 21:56:18 +0800

    update

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import path from 'path'
-import commonjs from '@rollup/plugin-commonjs'
+
 
 export default defineConfig({
   publicDir: false,
-  plugins: [commonjs()],
+
   build: {
     outDir: path.resolve(__dirname, 'dist/main'),
     lib: {
